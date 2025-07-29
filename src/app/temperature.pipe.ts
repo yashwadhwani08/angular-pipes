118491be565e2998c2c0aa691761d66c2fab47ea
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-29 17:35:47 +0530

    custom temperature pipe to transform the temperature value to be °F

---
+++ src/app/temperature.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'temp',
+  standalone: true,
+})
+export class TemperaturePipe implements PipeTransform {
+  transform(value: string | number) {
+    let val: number;
+
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    const outputTemp = val * (9 / 5) + 32; 
+    return `${outputTemp} °F`;
+  }
+}
